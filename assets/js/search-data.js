56e3ae7631708242259e19fcf265c683a3a1c346
Ariq Naufal <ariq@nauf.al>
2020-02-12 10:47:41 -0500

    commit pertama

---
+++ assets/js/search-data.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "[Part 1] Review Benchmark KS-9 Server",
+        "excerpt":"Penjelasan tentang hasil benchmark ini akan dibuat setelah post ini rilis.. (maklum sambil belajar).Thx Daftar Isi : Spesifikasi Lengkap Dedicated KS-9 di Kimsufi Testing Disk I/OPing Testing Disk DDBerikut hasil benchmark untuk KS-9 di Kimsufi.com (anak perusahaannya OVH) yang jual dedicated server murah : Kimsufi KS-9 Review BenchmarkBenchmark kali ini...","categories": ["review"],
+        "tags": ["review","benchmark","ovh","kimsufi"],
+        "url": "https://ariq.nauf.al/review/review-benchmark-dedicated-kimsufi-KS9/"
+      },{
+        "title": "Hello World",
+        "excerpt":"بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِIn the name of Allah, the Most Gracious, the Most Merciful Perkenalkan nama saya Ariq, saya masih belajar terutama dalam bidang cloud, linux, dan sebagainya.Sangat penting sebagai pemula seperti saya untuk mencatat pengalaman demi pengalaman, oleh karena itu dibuat blog ini.Selain untuk catatan diri saya pribadi, niatnya...","categories": ["blog"],
+        "tags": ["life"],
+        "url": "https://ariq.nauf.al/blog/hello-world/"
+      }]
