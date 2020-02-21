81426473c7a4ddee133844e6fd2f42866ab7c0d4
Ariq Naufal <ariq@nauf.al>
2020-02-21 17:11:56 +0000

    commit ulang krn salah config yml

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -2,20 +2,15 @@ var store = [{
         "title": "Hello World",
         "excerpt":"بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِIn the name of Allah, the Most Gracious, the Most Merciful Perkenalkan nama saya Ariq, saya masih belajar terutama dalam bidang cloud, linux, dan sebagainya.Sangat penting sebagai pemula seperti saya untuk mencatat pengalaman demi pengalaman, oleh karena itu dibuat blog ini.Selain untuk catatan diri saya pribadi, niatnya...","categories": ["blog"],
         "tags": ["life"],
-        "url": "http://kingcobra.nauf.al:4000/blog/hello-world/"
+        "url": "https://nulis.nauf.al/blog/hello-world/"
       },{
         "title": "Review Benchmark KS-9 Server Lengkap dengan Pembahasan",
         "excerpt":"Maaf jika masih ada yang belum sempurna pada bagian review dibawah, karena masih on going direview 1 1 (masih belajar hehe) Daftar Isi : Spesifikasi Lengkap Dedicated KS-9 di Kimsufi Testing IOPing Disk Testing DD Disk Testing FIO Disk Testing DD RAM Testing IOPing RAM Bandwidth Benchmark Ping BenchmarkBerikut hasil...","categories": ["review"],
         "tags": ["review","benchmark","ovh","kimsufi"],
-        "url": "http://kingcobra.nauf.al:4000/review/review-benchmark-dedicated-kimsufi-KS9/"
-      },{
-        "title": "Mudahnya Convert Audio Video ke Text dengan Sonix AI",
-        "excerpt":"بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِIn the name of Allah, the Most Gracious, the Most Merciful Daftar Isi : Daftar akun Sonix Login dan memulai Contoh upload video dari link YouTube Hasil transcribeJika kamu seorang youtuber yang ingin mengisikan caption di video kamu, seorang programmer yang penasaran bagaimana audio recognition bekerja, pembuat...","categories": ["blog"],
-        "tags": ["sonix","speech recognition","video","audio recognition","audio to text","transcribe"],
-        "url": "http://kingcobra.nauf.al:4000/blog/mudahnya-convert-audio-video-ke-text-dengan-sonix-ai/"
+        "url": "https://nulis.nauf.al/review/review-benchmark-dedicated-kimsufi-KS9/"
       },{
         "title": "Mudahnya Convert Audio Video ke Text dengan Sonix",
         "excerpt":"بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِIn the name of Allah, the Most Gracious, the Most Merciful Daftar Isi : Daftar akun Sonix Login dan memulai Contoh upload video dari link YouTube Hasil transcribeJika kamu seorang youtuber yang ingin mengisikan caption di video kamu, seorang programmer yang penasaran bagaimana audio recognition bekerja, pembuat...","categories": ["blog"],
         "tags": ["sonix","speech recognition","video","audio recognition","audio to text","transcribe"],
-        "url": "http://kingcobra.nauf.al:4000/blog/mudahnya-convert-audio-video-ke-text-dengan-sonix-gratis/"
+        "url": "https://nulis.nauf.al/blog/mudahnya-convert-audio-video-ke-text-dengan-sonix-ai/"
       }]
